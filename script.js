b9d112914063df6d8c371c0547f8b2c14a266f45
Demir Delic <demir.delic@gmail.com>
2017-10-02 18:10:26 -0600

    Add progress-bar functionality, clean up formatting

--- script.js
+++ script.js
@@ -1,91 +1,66 @@
 (function() {
 
-    var choices = ['rock', 'paper', 'scissors'];
+    var choices = ["rock", "paper", "scissors"];
     var index = Math.floor((Math.random() * 3));
     var computer = choices[index];
-    var rock = document.getElementById('rock');
-    var paper = document.getElementById('paper');
-    var scissors = document.getElementById('scissors');
+    var rock = document.getElementById("rock");
+    var paper = document.getElementById("paper");
+    var scissors = document.getElementById("scissors");
     var outcome = document.getElementById("outcomeAlert");
-    var progress = document.getElementsByClassName("progress-bar");
+    var progress = document.getElementsByClassName("progress-bar")[0];
     var playerScore = 0;
     var computerScore = 0;
 
-    function setIndex() {
+    function resetComputerChoice() {
         index = Math.floor((Math.random() * 3));
         computer = choices[index];
     }
 
-    rock.addEventListener('click', function() {
-        setIndex();
-        play('rock');
+    rock.addEventListener("click", function() {
+        resetComputerChoice();
+        play("rock");
     });
     
-    paper.addEventListener('click', function() {
-        setIndex();
-        play('paper');
+    paper.addEventListener("click", function() {
+        resetComputerChoice();
+        play("paper");
         
     });
     
-    scissors.addEventListener('click', function() {
-        setIndex();
-        play('scissors');
+    scissors.addEventListener("click", function() {
+        resetComputerChoice();
+        play("scissors");
         
     });
     
-    // Add your "play" function below this comment
     function play(player) {
         if (player === computer) {
-            outcome.innerHTML = (`Tie! <br> ${player} = ${computer} <br> Player score: ${playerScore} <br> Computer score: ${computerScore}`);
+            outcome.innerHTML = (`Tie! <br> 
+                                  ${player} = ${computer} <br> 
+                                  Player score: ${playerScore} <br> 
+                                  Computer score: ${computerScore}`);
             document.body.style = "background-color: #ffff00"; 
         }
         else if ((player === "rock" && computer === "scissors") || 
                  (player === "paper" && computer === "rock") || 
                  (player === "scissors" && computer === "paper")) {
             playerScore++;
-            outcome.innerHTML = (`You won! <br> ${player} > ${computer} <br> Player score: ${playerScore} <br> Computer score: ${computerScore}`);
+            outcome.innerHTML = (`You won! <br> 
+                                  ${player} > ${computer} <br> 
+                                  Player score: ${playerScore} <br> 
+                                  Computer score: ${computerScore}`);
             document.body.style = "background-color: #00ff00";
         }
         else {
             computerScore++;
-            outcome.innerHTML = (`You lost! <br> ${player} < ${computer} <br> Player score: ${playerScore} <br> Computer score: ${computerScore}`);
+            outcome.innerHTML = (`You lost! <br> 
+                                  ${player} < ${computer} <br> 
+                                  Player score: ${playerScore} <br> 
+                                  Computer score: ${computerScore}`);
             document.body.style = "background-color: #ff0000";
         }
-        progress
+        progress.style = `width: ${(playerScore / (playerScore + computerScore))*100}%`;
+        console.log(`${(playerScore / (playerScore + computerScore))*100}%`)
     }
 
 })();
-
-
-
-/*
-
-function play(player) {
-    var computer = Math.floor(Math.random()*3) //random number between 0 and 2, inclusive
-    var outcome = document.getElementById("outcomeAlert");
-    
-    function (choice) {
-      if(choice === 0) {
-        return "Rock";
-      }
-      else if(choice === 1) {
-        return "Paper";
-      }
-      else {
-        return "Scissors";
-      }
-    }
-    
-    if (player === computer)) {
-      outcome.innerHTML = ("The game resulted in a tie!");
-    }
-    else if ((player === "Rock" && computer) === "Scissors") || 
-             (player === "Paper" && computer) === "Rock") || 
-             (player === "Scissors" && computer) === "Paper")) {
-      outcome.innerHTML = ("You won!");
-    }
-    else {
-      outcome.innerHTML = ("You lost!");
-    }
-  }
-  */
